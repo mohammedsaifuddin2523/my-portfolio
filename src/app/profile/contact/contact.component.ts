57863eac5c7a25ee39d442c16870d883db7c00a6
Saifuddin Mohammed <Saifuddin.Mohammed@audreeinfotech.com>
2024-05-31 16:11:27 +0530

    CommitsSendEmailRemove

--- src/app/profile/contact/contact.component.ts
+++ src/app/profile/contact/contact.component.ts
@@ -28,36 +28,9 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
     
      }
-     sendEmail() {
-      debugger;
-      (window as any).Email.send({
-        
-        //SecureToken: "your-smtpjs-secure-token", // Generate a secure token from smtpjs.com
-        //SecureToken: "C973D7AD-F097-4B95-91F4-40ABC5567812",
-        Host : "smtp.elasticemail.com",
-        Username : "wipewen666@fresec.com", // Replace with your Elastic Email username
-        Password : "Audree@123", // Replace with your Elastic Email password
-        To: 'wipewen666@fresec.com',             // Replace with the recipient's email address
-        From: this.email.from_email,
-        Subject: `Contact Form: ${this.email.from_name}`,
-        Body: `Name: ${this.email.from_name}<br>Email: ${this.email.from_email}<br>Message: ${this.email.message}`,
-        Port: 587,                             // Port for non-SSL or 587 for TLS/SSL
-        UseDefaultCredentials: false,           // Do not use default credentials
-        EnableSsl: false  
-      }).then((message: string) => {
-        debugger;
-        alert('Email sent successfully');
-        //this.resetForm();
-        
-        // Optionally, display a success message to the user
-      }
-      , (error: any) => {
-        console.error('Failed to send email', error);
-        // Optionally, display an error message to the user
-      });
+     
       
-      
-    }
+    
     
    
 }
